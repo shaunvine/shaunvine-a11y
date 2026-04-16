5b4484b377cf9153f5e12c9cb7032c416c9d62f6
shaunvine <shaundvine@gmail.com>
2026-04-16 15:04:54 -0700

    Add projects components and fix GTM consent mode v2

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -31,11 +31,14 @@ export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
         __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
+
           gtag('consent', 'default', {
             ad_storage: 'denied',
             analytics_storage: 'denied',
-            personalization_storage: 'denied'
+            ad_user_data: 'denied',
+            ad_personalization: 'denied'
           });
+
           gtag('set', 'ads_data_redaction', true);
         `,
       }}
@@ -52,7 +55,8 @@ export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
             w[l]=w[l]||[];
             w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
             var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
             j.async=true;
             j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
             f.parentNode.insertBefore(j,f);
@@ -65,11 +69,15 @@ export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
   setPreBodyComponents([
     <noscript
       key="gtm-noscript"
-      aria-hidden="true" // hides from assistive tech
+      aria-hidden="true"
       dangerouslySetInnerHTML={{
         __html: `
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JMVV55D"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5JMVV55D"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
         `,
       }}
     />,
